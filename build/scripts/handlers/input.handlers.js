cc3af190110cbc9b7a6d9e7cc2d49cc06425c32c
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-05-04 15:30:49 -0300

    refact: improve the logic for updateCardCardNumber function

--- build/scripts/handlers/input.handlers.js
+++ build/scripts/handlers/input.handlers.js
@@ -1,4 +1,4 @@
-import { $spanCardHolder, $spanCardNumbers } from "../elements.js";
+import { $smallErrorCardNumber, $spanCardHolder, $spanCardNumbers, } from "../elements.js";
 export function updateCardCardHolder(e) {
     if (!e)
         return;
@@ -12,11 +12,21 @@ export function updateCardCardNumber(e) {
     if (!e)
         return;
     const inputElement = e.target;
-    const value = inputElement.value;
+    const value = Number(inputElement.value);
+    const isValueNaNOrEmpty = isNaN(value) || value === 0;
+    if (isValueNaNOrEmpty) {
+        if ($smallErrorCardNumber) {
+            $smallErrorCardNumber.classList.add("input-group__error-msg--visible");
+        }
+        return;
+    }
+    if ($smallErrorCardNumber) {
+        $smallErrorCardNumber.classList.remove("input-group__error-msg--visible");
+    }
     if ($spanCardNumbers) {
         const cardNumbersLength = $spanCardNumbers.length;
         for (let i = 0; i < cardNumbersLength; i++) {
-            $spanCardNumbers[i].textContent = value[i] || "0";
+            $spanCardNumbers[i].textContent = value.toString()[i] || "0";
         }
     }
 }
--- src/scripts/handlers/input.handlers.ts
+++ src/scripts/handlers/input.handlers.ts
@@ -1,4 +1,8 @@
-import { $spanCardHolder, $spanCardNumbers } from "../elements.js";
+import {
+  $smallErrorCardNumber,
+  $spanCardHolder,
+  $spanCardNumbers,
+} from "../elements.js";
 
 export function updateCardCardHolder(e: Event) {
   if (!e) return;
@@ -12,13 +16,27 @@ export function updateCardCardHolder(e: Event) {
 
 export function updateCardCardNumber(e: Event) {
   if (!e) return;
+
   const inputElement = e.target as HTMLInputElement;
-  const value = inputElement.value;
+  const value = Number(inputElement.value);
+  const isValueNaNOrEmpty = isNaN(value) || value === 0;
+
+  if (isValueNaNOrEmpty) {
+    if ($smallErrorCardNumber) {
+      $smallErrorCardNumber.classList.add("input-group__error-msg--visible");
+    }
+
+    return;
+  }
+
+  if ($smallErrorCardNumber) {
+    $smallErrorCardNumber.classList.remove("input-group__error-msg--visible");
+  }
 
   if ($spanCardNumbers) {
     const cardNumbersLength = $spanCardNumbers.length;
     for (let i = 0; i < cardNumbersLength; i++) {
-      $spanCardNumbers[i].textContent = value[i] || "0";
+      $spanCardNumbers[i].textContent = value.toString()[i] || "0";
     }
   }
 }
